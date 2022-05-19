49ed9c0aaf55a447ae43b549083cd069fdc98c82
William Berglund <william.berglund@pm.me>
2022-05-19 10:28:34 +0100

    Post-Flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+
+  description = "Flake for Dashbored";
+
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-2111";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system:
+
+      let
+
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            dashbored = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
+              shell.tools = {
+                cabal = { };
+                hlint = { };
+                ormolu = { };
+                haskell-language-server = { };
+              };
+              shell.buildInputs = [ pkgs.nixpkgs-fmt ];
+            };
+          })
+        ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+
+        flake = pkgs.dashbored.flake { };
+
+      in flake // {
+        defaultPackage = flake.packages."dashbored:exe:dashbored";
+      });
+
+}
--- shell.nix
+++
@@ -1,8 +0,0 @@
-(import ./default.nix).shellFor {
-  tools = {
-    cabal = "latest";
-    hlint = "3.2.7";
-    ormolu = "0.1.4.1";
-    haskell-language-server = "latest";
-  };
-}
